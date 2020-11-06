e26e62f2bc0a4eb1e4b2afdb6c3dbdafa6f6eed8
zhangjing-github-code <zhangjing20091105@126.com>
2020-11-06 18:20:58 +0800

    first of bubbleSort

---
+++ bubblesort/bubblesort.py
@@ -0,0 +1,13 @@
+def bubbleSort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
+	    if (arr[j] < arr[j+1]):
+		arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+def arrMaker(a,b,qty):
+    import random
+    arr=[]
+    for i in range(qty):
+	arr.append(random.randint(a,b))
+    return arr
