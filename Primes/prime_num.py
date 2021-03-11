dc38bbf3ebc4c150f5ceb1efc35433d30161470b
zhangjing-github-code <zhangjing20091105@126.com>
2021-03-11 18:36:35 +0800

    commit for completely nums (a bug :))

--- Primes/prime_num.py
+++ Primes/prime_num.py
@@ -11,4 +11,21 @@ for j in range(2,int(bignum)):
     if isPrime(j):
         prime_nums.append(j)
 print("primes less than",bignum,prime_nums)
-
+def completenum(num):
+    yinshu=[]
+    for i in range (2,num-1):
+        if num % i == 0:
+            yinshu.append(i)
+    summ=0
+    for i in range(len(yinshu)):
+        #global summ
+        summ+=yinshu[i]
+    if summ == num :
+        return True
+    else:
+        return False
+comp_nums=[]
+for j in range(1000):
+    if completenum(j):
+        comp_nums.append(j)
+print(comp_nums)
